b2e7025a0fae41bd78edccb38bbae484148b25f8
delightlayman <163109448+delightlayman@users.noreply.github.com>
2025-04-11 19:45:56 +0800

    add

--- vector.cpp
+++ vector.cpp
@@ -3,10 +3,18 @@
 namespace MY_STL{
     //constructor
     template<class T>
-    m_vector<T>::m_vector(){}
+    m_vector<T>::m_vector()
+        :_start(nullptr),_finish(nullptr),_end_of_storage(nullptr){}
 
     template<class T>
-    m_vector<T>::m_vector(T *arr){}
+    m_vector<T>::m_vector(T* arr){
+        size_t len=sizeof(arr)/sizeof(T);
+        T* tmp=new T[len];
+        memcpy(tmp,arr,sizeof(arr));
+        _start=tmp;
+        _finish=tmp+len;
+        _end_of_storage=tmp+len; 
+    }
 
     template<class T>
     m_vector<T>::m_vector(const m_vector<T>& a){}
@@ -20,8 +28,37 @@ namespace MY_STL{
 
     //operator =
     template<class T>
-    const m_vector<T>& m_vector<T>::operator=(const m_vector<T>& a){}
+    m_vector<T>& m_vector<T>::operator=(const m_vector<T>& a){}
     
     template<class T>
-    const m_vector<T>& m_vector<T>::operator=(m_vector<T>&& a){}
+    m_vector<T> m_vector<T>::operator=(m_vector<T>&& a){}
+
+
+    //operator []
+    template< class T>
+    T& m_vector<T>::operator[](size_t pos){}
+    
+    template< class T>
+    const T& m_vector<T>::operator[](size_t pos)const{}
+
+    //adjust size and capacity
+    template< class T>
+    void m_vector<T>::resize(size_t n){}
+    template< class T>
+    void m_vector<T>::reserve(size_t n){
+        //注意：新旧数组指针间的关系
+        size_t si=size();
+        size_t cap=capacity();
+
+        if(n>cap){
+            T* tmp=new T[n];
+            if(_start){
+                memcpy(tmp,_start,sizeof(T)*si);
+                delete[] _start;
+            }
+            _start=tmp;
+            _finish=_start+si;
+            _end_of_storage=_start+cap;
+        }
+    }
 }
--- vector.h
+++ vector.h
@@ -4,21 +4,60 @@
 namespace MY_STL{
     template <class T>
     class m_vector{
+        typedef T* iterator;
+
         private:
             iterator _start;
             iterator _finish;
             iterator _end_of_storage;
         public:
+            //iterator
+            iterator begin(){
+                return _start;
+            }
+            iterator end(){
+                return _finish;
+            }
             //constructor
             m_vector();
-            explicit m_vector(int *arr);
+            m_vector(size_t len,const T& val);
+            explicit m_vector(T* arr);
             m_vector(const m_vector& a);
             m_vector(m_vector&& a);
             //destructor
             ~m_vector();
             //operator =
-            operator=(const m_vector& a);
-            operator=(m_vector&& a);
+            m_vector& operator=(const m_vector& a);
+            m_vector operator=(m_vector&& a);
+            //operator []
+            T& operator[](size_t pos);
+            const T& operator[](size_t pos)const;
+            
+            //size and capacity 
+            size_t size(){
+                return _finish-_start;
+            }
+            size_t capacity(){
+                return _end_of_storage-_start;
+            }
+
+            //adjust size and capacity
+            void resize(size_t n);
+            void reserve(size_t n);
+            
+            //add
+            m_vector& push_back(const T& n);
+            m_vector& insert(size_t pos,T& n);
+            m_vector& insert(size_t pos,T* arr);
+            m_vector& insert(size_t pos,const m_vector& n);
+            //delete
+            m_vector& pop_back();
+            m_vector& erase(size_t pos,size_t len); 
+
+            //query
+            iterator find(T& n);
+            //modify
+            
 
     };
 }
